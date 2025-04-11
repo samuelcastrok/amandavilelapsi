fbc2b88e267b0b8e4a194b4591010d55bfb56c30
Samuel Castro <samuel.castro@golfleet.com.br>
2025-04-11 19:32:30 -0300

    chore: add open graph

--- app/_components/Services.tsx
+++ app/_components/Services.tsx
@@ -7,13 +7,13 @@ export default function Services(props: {id: string}) {
         <hr className="w-full bt-1 border-secondary-color" />
       </header>
       <div className="flex flex-col items-center sm:flex-row gap-10 sm:gap-16 justify-center h-full">
-        <div className="group text-lg leading-6  text-justify rounded-lg overflow-hidden max-w-sm h-full">
+        <div className="group text-lg leading-6  rounded-lg overflow-hidden max-w-sm h-full">
           <div className="flex flex-col gap-4 bg-secondary-color text-secondary-text-color color p-4 h-full">
             <h2 className="font-pritude-radiance text-4xl">Presencial</h2>
             <p className="text-base">Ofereço atendimento presencial na cidade de Londrina, no Núcleo Evoluir. As sessões têm duração de 50 minutos, e para tornar esse momento ainda mais agradável, temos um cafézinho delicioso. :)</p>
           </div>
         </div>
-        <div className="group text-lg leading-6  text-justify border border-secondary-color rounded-lg overflow-hidden max-w-sm h-full">
+        <div className="group text-lg leading-6  border border-secondary-color rounded-lg overflow-hidden max-w-sm h-full">
           <div className="flex flex-col gap-4 bg-secondary-color text-secondary-text-color p-4 h-full">
             <h2 className="font-pritude-radiance text-4xl">Online</h2>
             <p className="text-base">Para aqueles que não residem em Londrina ou preferem a comodidade do atendimento remoto, as sessões são conduzidas através do Google Meet, com a mesma duração de 50 minutos.</p>
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,21 @@ const quicksand = Quicksand({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Amanda Vilela",
   description: "Amanda Vilela - Psicoterapia",
+  openGraph: {
+    title: "Amanda Vilela", 
+    description: "Amanda Vilela - Psicoterapia",
+    url: "https://amandavilela.com",
+    images: [
+      {
+        url: "./public/images/favicon.svg",
+        width: 800,
+        height: 600,
+      },
+    ],
+    siteName: "Amanda Vilela",
+    type: "website",
+    locale: "pt-BR",
+  },
 };
 
 export default function RootLayout({
@@ -19,6 +34,10 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.svg" />
+        <meta property="og:title" content="The Rock" />
+        <meta property="og:type" content="video.movie" />
+        <meta property="og:url" content="https://www.imdb.com/title/tt0117500/" />
+        <meta property="og:image" content="https://ia.media-imdb.com/images/rock.jpg" />
       </head>
       <body className={quicksand.className}>
         <Header />
